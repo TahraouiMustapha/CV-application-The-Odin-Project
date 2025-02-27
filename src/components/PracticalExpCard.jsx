11fe2d51cfcfb9109aa3f4c546956d999a0a244b
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2025-02-27 19:08:28 +0100

    feat : add handle submition function to practical experience card

--- src/components/PracticalExpCard.jsx
+++ src/components/PracticalExpCard.jsx
@@ -1,20 +1,39 @@
+import { useState } from "react"
 
-export function PracticalExperienceCard() {
+export function PracticalExperienceCard(props) {
+    const [inputValues, setInputValues] = useState({
+        companyName:'',
+        position: '',
+        fromDate: '',
+        toDate: '',
+        details: ''
+    })
+
+
     return (
         <div>
-            <form>
+            <form onSubmit={(e)=> {
+                e.preventDefault()
+                props.handleExperiences(inputValues)
+            }}>
                 <p>
                     <label htmlFor="company-name">Company Name: </label>
                     <input type="text"
                     name="company-name"
-                    id="company-name" />
+                    id="company-name" 
+                    onChange={(e) => {
+                        setInputValues({...inputValues, companyName: e.target.value});
+                    }}/>
                 </p>
                 
                 <p>
                     <label htmlFor="position">Position: </label>
                     <input type="text"
                     name="position"
-                    id="position" />
+                    id="position" 
+                    onChange={(e)=>{
+                        setInputValues({...inputValues, position: e.target.value})
+                    }}/>
                 </p>
 
                 <div className="career-time">
@@ -22,20 +41,32 @@ export function PracticalExperienceCard() {
                         <label htmlFor="from-date">From: </label>
                         <input type="date"
                         name="from-date"
-                        id="from-date" />
+                        id="from-date" 
+                        onChange={(e)=>{
+                            setInputValues({...inputValues, fromDate: e.target.value})
+                        }}/>
                     </p>
 
                     <p>
                         <label htmlFor="to-date">To: </label>
                         <input type="date"
                         name="to-date"
-                        id="to-date" />
+                        id="to-date" 
+                        onChange={(e)=>{
+                            setInputValues({...inputValues, toDate: e.target.value})
+                        }}/>
                     </p>
                 </div>
 
                 <p>
                     <label htmlFor="details">Details: </label>
-                    <textarea name="details" id="details"></textarea>
+                    <textarea name="details" id="details"
+                    onChange={(e)=>{
+                        setInputValues({
+                            ...inputValues, 
+                            details: e.target.value
+                        })
+                    }}></textarea>
                 </p>
 
                 <button>submit</button>
