1e3a076d00182847973dbff07a6b742e14a51fd8
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2025-02-27 15:53:04 +0100

    remove profession input

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -4,7 +4,6 @@ export function PersonalDetails(props) {
     const [inputValues, setInputValues] = useState({
         fullName: '', 
         email: '', 
-        profession: '', 
         phoneNumber: ''
     })
 
@@ -14,7 +13,6 @@ export function PersonalDetails(props) {
 
         if ( myInput === 'email') newInputValuesObj = {...inputValues, email: e.target.value}
         else if (myInput === 'full-name') newInputValuesObj = {...inputValues, fullName: e.target.value}
-        else if (myInput === 'profession') newInputValuesObj = {...inputValues, profession: e.target.value}
         else newInputValuesObj = {...inputValues, phoneNumber: e.target.value}
 
         setInputValues(newInputValuesObj)
@@ -39,14 +37,6 @@ export function PersonalDetails(props) {
                         <input type="email" 
                         name="email" 
                         onChange={handleChangeValues}/>
-                    </p>
-
-                    <p>
-                        <label htmlFor="profession">Profession: </label>
-                        <input type="text" 
-                        name="profession"
-                        onChange={handleChangeValues}
-                        />
                     </p>
 
                     <p>
